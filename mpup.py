caece39fcf8d6411c876186785c39c83cadf688b
Beau Barker <beauinmelbourne@gmail.com>
2016-09-02 06:03:24 +1000

    Refactored for click groups

--- mpup.py
+++ mpup.py
@@ -1,30 +1,49 @@
-"""Upload file to Micropython"""
+"""Upload files to Micropython"""
+import logging
+from os.path import basename
 import serial
-import time
 import click
 
-@click.command()
-@click.option('--port', '-p', default='/dev/ttyUSB0', help='serial port device')
-@click.option('--baud', '-b', default=115200, help='port speed in baud')
-@click.option('--delay', '-d', default=100.0, help='delay between lines (ms)')
-@click.option('--interrupt/--no-interrupt', '-i', help='send soft interrupt (ctrl-c) before upload')
-@click.option('--reset/--no-reset', '-r', help='send soft reset (ctrl-d) after upload')
-@click.argument('files', nargs=-1, type=click.File('rb'))
-def cli():
-    """Main cli entrypoint"""
-    port = serial.Serial(args.port, args.baud)
-    if args.interrupt:
-        port.write(bytes('\x03', 'ascii'))
-    for fh in args.files:
-        port.write(bytes('_fh = open(%s, "w")\r' % repr(fh.name), 'ascii'))
-        while True:
-            s = fh.read(50)
-            if len(s) == 0:
-                break
-            port.write(bytes('_fh.write(%s)\r' % repr(s), 'ascii'))
-            time.sleep(args.delay/1000.0)
-        port.write(bytes('_fh.close()\r', 'ascii'))
-        fh.close()
-    if args.reset:
-        port.write(bytes('\x04', 'ascii'))
-    port.close()
+logger = logging.getLogger(__name__)
+
+
+def send(ser, s):
+    """Send message to serial device"""
+    ser.write(b'%b\r' % s.encode())
+    logger.debug(s)
+
+
+@click.group()
+@click.option('-b', '--baud', metavar='<rate>', default=115200, \
+        help='Port speed in baud.')
+@click.option('-d', '--delay', metavar='<delay>', default=100.0, \
+        help='Delay between lines (ms).')
+@click.option('-i', '--interrupt', is_flag=True, \
+        help='Send soft interrupt (ctrl-c) before command.')
+@click.option('-p', '--port', metavar='<port>', default='/dev/ttyUSB0', \
+        help='Serial port device.')
+@click.pass_context
+def cli(ctx, baud, delay, interrupt, port):
+    """Manage files with Micropython"""
+    port = serial.Serial(port, baud, timeout=1)
+    if interrupt:
+        send(port, '\x03')
+    ctx.obj = {'port': port, 'delay': delay}
+
+
+@cli.command()
+@click.option('-r', '--reset', is_flag=True, \
+        help='Send soft reset (ctrl-d) after upload.')
+@click.argument('files', nargs=-1, required=True, type=click.File('rb'))
+@click.pass_obj
+def upload(ctx, reset, files):
+    """Upload files to Micropython"""
+    for localfile in files:
+        with open(localfile.name) as lf:
+            s = lf.read()
+        send(ctx['port'], \
+            'with open({}, "wb") as remotefile: remotefile.write({})\n\r' \
+            .format(repr(basename(localfile.name)), repr(s)))
+    if reset:
+        send(ctx['port'], '\x04')
+    ctx['port'].close()
