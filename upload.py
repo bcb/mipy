33bf95942259167ef84fdbd008bfa98627428735
Beau Barker <beauinmelbourne@gmail.com>
2016-09-02 00:43:35 +1000

    Clean up formatting

--- upload.py
+++ upload.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import serial
 import time
 import argparse
@@ -15,25 +14,23 @@ parser.add_argument('--reset', action='store_true', help='send soft reset (contr
 parser.add_argument('files', nargs='*', type=argparse.FileType('rb'))
 args = parser.parse_args()
 
-port = serial.Serial(args.port, args.baud)
+def main():
+    port = serial.Serial(args.port, args.baud)
+    if args.interrupt:
+        port.write(bytes('\x03', 'ascii'))
+    for fh in args.files:
+        port.write(bytes('_fh = open(%s, "w")\r' % repr(fh.name), 'ascii'))
+        while True:
+            s = fh.read(50)
+            if len(s) == 0:
+                break
+            port.write(bytes('_fh.write(%s)\r' % repr(s), 'ascii'))
+            time.sleep(args.delay/1000.0)
+        port.write(bytes('_fh.close()\r', 'ascii'))
+        fh.close()
+    if args.reset:
+        port.write(bytes('\x04', 'ascii'))
+    port.close()
 
-if args.interrupt:
-  port.write('\x03')
-
-for fh in args.files:
-
-  port.write(bytes('_fh = open(%s, "w")\r' % repr(fh.name), 'ascii'))
-
-  while True:
-    s = fh.read(50)
-    if len(s) == 0: break
-    port.write(bytes('_fh.write(%s)\r' % repr(s), 'ascii'))
-    time.sleep(args.delay/1000.0)
-
-  port.write(bytes('_fh.close()\r', 'ascii'))
-  fh.close()
-
-if args.reset:
-  port.write('\x04')
-
-port.close()
+if __name__ == '__main__':
+    main()
